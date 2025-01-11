5a33319678207165fbaec1950138c4fb0bb80022
Timilehin Daramola <timi.homezone111@gmail.com>
2025-01-10 20:53:42 -0500

    CSE340

--- server.js
+++ server.js
@@ -6,9 +6,18 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
 
 /* ***********************
  * Routes
@@ -25,6 +34,11 @@ const host = process.env.HOST
 /* ***********************
  * Log statement to confirm server operation
  *************************/
+
+app.get("/", (req, res) => {
+  res.status(200).render("./layouts/layout", {title: "Home Page"})
+})
+
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
